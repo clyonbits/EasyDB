a652d0fb1f364004713e482b3bed421867dd667c
clyonbits <clyonbytes@gmail.com>
2023-02-11 17:17:46 -0500

    Changed 'post' method to 'create' method

--- easydb.py
+++ easydb.py
@@ -3,7 +3,7 @@ class EasyDB:
     def __init__(self, name):
         self.name = name
         self.filename = f"{self.name}.{EXTENSION}"
-    def post(self, key, value):
+    def create(self, key, value):
         with open(self.filename, "w") as db:
             db.write(f"{key}:{value}")
     def get(self, key):
